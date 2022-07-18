baef783baca67aa52e8f0f77a0bfa2367fbb2fdb
alvaroun <alvaromachadoferreira@hotmail.com>
2022-07-17 23:46:11 -0300

    deploy test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutMeComponent } from './pages/about-me/about-me.component';
 
 const routes: Routes = [
-  { path: 'Home', component: HomeComponent },
-  { path: 'AboutMeComponent', component: AboutMeComponent },
+  { path: '', component: HomeComponent },
+  { path: 'about-me', component: AboutMeComponent },
 ];
 
 @NgModule({
