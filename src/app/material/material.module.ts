db1d1f2346260f9552ff9f8f8609c8ccda18b597
Alvaro Ferreira <alvaromachadoferreira@hotmail.com>
2022-11-04 19:01:38 -0300

    feat: import divider module

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,14 +1,15 @@
+import { CdkAccordionModule } from '@angular/cdk/accordion';
 import { NgModule } from '@angular/core';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatTableModule } from '@angular/material/table';
-import { MatButtonModule } from '@angular/material/button';
-import { CdkAccordionModule } from '@angular/cdk/accordion';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
 @NgModule({
@@ -23,6 +24,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
     MatButtonModule,
     CdkAccordionModule,
     MatProgressSpinnerModule,
+    MatDividerModule
 
   ]
 })
