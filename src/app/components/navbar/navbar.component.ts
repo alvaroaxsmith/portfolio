6ef541aac07c87d48f584b5f7301f4ed3dd34270
Alvaro Ferreira <alvaromachadoferreira@hotmail.com>
2025-06-28 20:16:47 -0300

    feat: update navbar component with scroll behavior and styling adjustments

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,28 +1,51 @@
 import { Dialog } from './dialog/dialog.component';
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding, HostListener, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  private lastScrollY = 0;
+
+  @HostBinding('class.navbar-fixed') navbarFixed = false;
+  @HostBinding('class.navbar-hidden') navbarHidden = false;
+
   constructor(
     public dialog: MatDialog,
     public translate: TranslateService // Manter TranslateService injetado para uso
-  ) {
-  }
+  ) {}
 
   switchLang(lang: string) {
     this.translate.use(lang);
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollY = window.scrollY;
+    const isScrollingDown = scrollY > this.lastScrollY;
+    const isBeyondThreshold = scrollY > window.innerHeight * 0.2;
+
+    this.navbarFixed = scrollY > 0;
+
+    if (isScrollingDown && isBeyondThreshold) {
+      this.navbarHidden = true;
+    } else if (!isScrollingDown) {
+      this.navbarHidden = false;
+    }
+
+    this.lastScrollY = scrollY;
   }
 
   openDialog() {
-    this.dialog.open(Dialog).afterClosed().subscribe(() => { });
+    this.dialog
+      .open(Dialog)
+      .afterClosed()
+      .subscribe(() => {});
   }
 }
